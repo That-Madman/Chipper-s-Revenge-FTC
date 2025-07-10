95de9ebb8a40b02f2fa56042a888159a9a0a3035
That-Madman <144173821+That-Madman@users.noreply.github.com>
2025-07-10 13:04:13 -0400

    Added LynxModule automatic bulk reading.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Teleop.java
@@ -1,16 +1,27 @@
 package org.firstinspires.ftc.teamcode.opModes;
 
+import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.subsystems.trailblazer.drivebase.Drive;
 
+import java.util.List;
+
 @TeleOp
 public class Teleop extends LinearOpMode {
     Drive drive;
 
+    List<LynxModule> allHubs;
+
     @Override
     public void runOpMode() throws InterruptedException {
+        allHubs = hardwareMap.getAll(LynxModule.class);
+
+        for (LynxModule hub : allHubs) {
+            hub.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        }
+
         drive = new Drive(hardwareMap);
 
         waitForStart();
